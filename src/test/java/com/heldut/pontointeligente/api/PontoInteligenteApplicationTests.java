fb32afbc5927ed42f0eb9842b2a53d7e51157ca9
Heldut <hel_dut@hotmail.com>
2023-08-21 12:10:40 -0300

    Commit inicial dos arquivos do projeto

---
+++ src/test/java/com/heldut/pontointeligente/api/PontoInteligenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heldut.pontointeligente.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoInteligenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
